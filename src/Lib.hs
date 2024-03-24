1722b271b6e0d2c04dc18e26a65c1583c7a509a5
Arthurabriel <arthur.gabriel.santos.albuquerque@ccc.ufcg.edu.br>
2024-03-24 12:19:55 -0300

    SalvandoConquista

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,9 @@
 module Lib where
 
 import System.Process
+import Models.Conquista
+import Models.Player
+import System.IO
 
 textoFormatado::String->String
 textoFormatado texto = texto ++ "\n-----------------------------------------------------------------------------------------------------------------------------------------------------------------------\n"
@@ -9,4 +12,44 @@ clearScreen::IO()
 clearScreen = do
     _ <- system "clear"
     putStr "\ESC[H"
-    
+
+carregaPlayer :: IO Player
+carregaPlayer = do
+        handle <- openFile "./pacote/Heroi.txt" ReadMode
+        conteudo <- hGetContents' handle
+        hClose handle
+        return (read conteudo :: Player)
+
+salvaConquista :: [Conquista] -> IO ()
+salvaConquista conquistas = writeFile "./src/pacote/Conquista.txt" $ unlines (map show conquistas)
+
+desbloqueaConquista :: String -> IO()
+desbloqueaConquista nomeConquista = do
+    conquistas <- carregaConquista  
+    let conquistaDesbloqueda = desbloquearConquista nomeConquista conquistas
+    salvaConquista conquistaDesbloqueda
+
+carregaConquista :: IO [Conquista]
+carregaConquista = do
+        handle <- openFile "./src/pacote/Conquista.txt" ReadMode
+        conteudo <- hGetContents' handle
+        hClose handle
+        return (map parseConquista (lines conteudo))
+
+
+desbloquearConquista :: String -> [Conquista] -> [Conquista]
+desbloquearConquista nomeConquista =
+     map (\c -> if nomeC c == nomeConquista then c { alcancou = True } else c)
+
+parseConquista :: String -> Conquista
+parseConquista texto = read texto :: Conquista
+
+fechaJogo :: String
+fechaJogo = "Você fugiu covardemente...Boa sorte em ensinar o Teorema do resto chinês."
+
+esperandoEnter :: IO ()
+esperandoEnter = do
+    putStr "Pressione Enter para continuar..."
+    hFlush stdout
+    _ <- getLine
+    return ()
--- src/Main.hs
+++ src/Main.hs
@@ -15,35 +15,47 @@ main = do
 
     putStrLn inicioHistoria
     escolha <- getLine
-    escolha1 escolha
+    escolha1 escolha 0
 
-escolha1::String->IO()
-escolha1 escolha01 = do
+escolha1::String-> Int -> IO()
+escolha1 escolha vezes_negado = do
 
     clearScreen
 
-    if escolha01 == "1" then do
-        putStrLn cursoParte01
-        setupInicial
+    case escolha of
+        "1" -> irSetupInicial
 
-    else if escolha01 == "2" then do 
-        putStrLn "Você negou-se a ir?? Isso foi o limite de faltas...Seja jubilado desse mundo."
-        putStrLn fechaJogo --Aguardando interacao de conquista.
+        "2" -> jubilado (vezes_negado + 1)
 
-    else do
-        putStrLn "A liberdade de expressão era uma mentira..."
-        putStrLn cursoParte01
-        setupInicial 
-    
+
+        _ -> do
+            putStrLn "A liberdade de expressão era uma mentira..."
+            irSetupInicial
+
+jubilado :: Int -> IO ()
+jubilado 8 = serJubilado
+jubilado vezes_negado = do
+    putStrLn "Pense melhor sobre isso professor..."
+    putStrLn $ "(1) Sim, quero ajudar.\n" ++ "(2) " ++ concat (replicate vezes_negado "não ")
+    escolha <- getLine
+    escolha1 escolha vezes_negado
+
+serJubilado :: IO()
+serJubilado = do
+    putStrLn $ "Heanes: Quantas vezes vou ter que repetir eu não quero fazer isso\n"
+     ++ "Carl Wilson: Então você escolheu isso... não me culpe meu amigo\n *você foi ejetado para o limbo*"
+    desbloqueaConquista "Jubilado"
+
+irSetupInicial::IO ()
+irSetupInicial = do
+    putStrLn cursoParte01
+
 setupInicial::IO()
 setupInicial = do
-    heroi <- readFile' "dia01/src/pacote/Heroi.txt"
+    heroi <- readFile' "./src/pacote/Heroi.txt"
     let heanes = read heroi :: Models.Player.Player
     putStrLn (textoFormatado "...")
     escolha2
-
-fechaJogo::String
-fechaJogo = "Você fugiu covardemente...Boa sorte em ensinar o Teorema do resto chinês."
 
 menuInicial::String
 menuInicial = "O que deseja fazer??\n(1) Explorar a cidade.\n(2) Seguir Carl Wilson."
