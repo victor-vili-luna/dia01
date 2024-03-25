19cba85f013181edd6fe308fefe73506eff7b5ca
dsmaufcg <diego-souto-maior-aleixo@ccc.ufcg.edu.br>
2024-03-25 15:27:06 -0300

    Trabalho DURO

--- src/Loja.hs
+++ src/Loja.hs
@@ -11,16 +11,17 @@ abreLojaItensInicial::IO()
 abreLojaItensInicial = do
     clearScreen
 
-    putStrLn (textoFormatado "Aqui está uma lista de itens que atualmente voce pode comprar se tiver o dinheiro: ")
+    putStrLn (textoFormatado "Ferreira, o ferreiro: Olá herói! Aqui está uma lista de itens que você pode comprar caso tenha as moedas, claro:\n")
     arquivo02 <- readFile' "./src/pacote/ItensIniciais.txt"
     let lojaItem = map (read::String->Item) (lines arquivo02)
     print lojaItem
+    compraItem item
 
 abreLojaPocoesInicial::IO()
 abreLojaPocoesInicial = do
     clearScreen
 
-    putStrLn (textoFormatado "Aqui está uma lista de pocoes que atualmente voce pode comprar se tiver o dinheiro: ")
+    putStrLn (textoFormatado "Ferreira, o ferreiro: Olá herói! Aqui está uma lista de poções que você pode comprar caso tenha as moedas, claro:\n")
     arquivoPocaoInicial <- readFile' "./src/pacote/PocaoInicial.txt"
     let pocao = read arquivoPocaoInicial :: Pocao
     print pocao
@@ -29,6 +30,24 @@ abreLojaPocoesInicial = do
     if input == "1" then compraPocao pocao
     else putStrLn "Não quer comprar hein...tudo bem."
 
+compraItem::Item->IO()
+compraItem item = do
+    putStrLn "Digite o nome do item que você deseja comprar. Caso você esteja liso e não queira comprar nada, digite SAIR."
+    input <- getLine
+    if input == "SAIR" then do
+        putStrLn "ferreiro Ferreira: Não quer comprar hein... Tudo bem."
+    else if input == Models.Item.nome item then do
+        arquivoHeroi <- readFile' "./src/pacote/Heroi.txt"
+        let heanesPre = read arquivoHeroi :: Player
+            gold = Models.Player.gold heanesPre
+        if gold >= Models.Item.preco item then do
+            let goldAtual = Models.Player.gold heanesPre - Models.Item.preco item
+                heanesAdulto = heanesPre { gold = goldAtual, equipamentos = equipamentos heanesPre ++ [item] }
+            writeFile "./src/pacote/Heroi.txt" (show heanesAdulto)
+        else putStrLn "Pobretão, quer adquirir mais algum produto?"
+    else do 
+        putStrLn "Por favor digite novamente."
+        compraItem item
 
 compraPocao::Pocao->IO()
 compraPocao pocao = do
@@ -49,10 +68,12 @@ compraPocao pocao = do
 
 verLoja::IO()
 verLoja = do
-    putStrLn "Esses são os itens e os preços que quando você trabalhar, poderá comprar: "
+    putStrLn "Esses são os itens e os preços que quando você trabalhar, poderá comprar: \n"
     arquivo02 <- readFile' "./src/pacote/ItensIniciais.txt"
     let loja = map (read::String->Item) (lines arquivo02)
     print loja
+    putStrLn "\nMas como você ainda não possui nenhum tostão, acho melhor você pensar em fazer alguma outra coisa..."
+    putStrLn "\n------------------------------------------------------------------------------------\n"
 
 
 
--- src/MundoCidadela.hs
+++ src/MundoCidadela.hs
@@ -8,33 +8,29 @@ import System.IO (readFile')
 import Lib
 
 dialogoPadeiro01::String
-dialogoPadeiro01 = textoFormatado "\nVocê encontra uma padaria no caminho e decide conversar com o padeiro...\nPadeiro: Você não notou nada de estranho Héroi?? Por que so você pode derrotar a I.A., e se assim, por que não tem todos os melhores itens?, tenha cuidado meu caro...\nHeanes: O que quer dizer com isso??\nPadeiro: Eu já falei demais... \n*Misterioresamente o homem que estava lá, desaparece aos olhos do professor.*\nO professor decide ignorar isso por enquanto, por enquanto."
+dialogoPadeiro01 = textoFormatado "\n*Você encontra a Panificadora Alfa no caminho e decide conversar com o padeiro...*\n\nPadeiro: HoHoHo, Você não notou nada de estranho, Héroi?? Tenha cuidado, nem todos na Cidadela de Critais são confiáveis...\n\nHeanes: O que quer dizer com isso??\nPadeiro: Lembre-se disso: लोहार पर भरोसा मत करो... \n\n*Misterioresamente o homem que estava lá, desaparece aos olhos do professor.*\n\n*O professor decide ignorar isso por enquanto. Por enquanto...*"
 
 dialogoFerreira01::String
-dialogoFerreira01 = textoFormatado "\nHeanes: Tá mas se eu vou salvar o mundo por que você só não me dá os melhores itens de uma vez? Ferreira: *cerra os olhos e diz* Amigos amigos negócios à parte meu colega."
-
-dialogoTrabalho01::String
-dialogoTrabalho01 = textoFormatado "\nOlá meu amigo, vejo que C.W. o mandou para mim, bem, o que posso fazer por você??\nHeanes: Estou precisando de dinheiro para comprar algumas coisas...\nEntendo, entendo, bom. Existem duas formas:\n"
+dialogoFerreira01 = textoFormatado "\nHeanes: Ferreira, se eu vou salvar o mundo, por que você só não me dá os melhores itens de uma vez?\n\nFerreira: *cerra os olhos e diz* Negócios são negócios, como vou pagar o meu café que tomo em seu Élho?\n\n*Ferreira não está mais aberto a conversas, acho melhor fazer alguma outra coisa decente*\n"
 
 explicacaoBasica01::String
-explicacaoBasica01 = textoFormatado "\nEntão me ajude a carregar essas coisas aqui e dependendo de quantas vc levar, eu pago a quantia adequada."
-
-explicacaoBasica02::String
-explicacaoBasica02 = textoFormatado "\nEntendo entendo, pelo meio difícil hein, então responda a seguinte pergunta:"
+explicacaoBasica01 = textoFormatado "\nC.W.: Ótimo! Então me ajude a carregar essas caixas, e dependendo de quantas você levar, eu pago a quantia adequada.\n"
 
 cursoAventura01::String
-cursoAventura01 = textoFormatado "\nC.W.: Bom, agora que você conhece as mecânicas básicas do jogo, recomendo ir atrás de algumas informações sobre as I.As, tudo que eu sei sobre a sua primeira I.A. é que você pode ser ajudado pelo Leandro.\nHeanes: Que Leandro??\nC.W.:Da vinci."
+cursoAventura01 = textoFormatado "\nC.W.: Bom, agora que você já está mais orientado nessa bela cidadela, recomendo ir atrás de algumas informações sobre as I.As, Quem pode te ajudar nisso é o Leandro.\n\nHeanes: Que Leandro??\n\nC.W.: Leandro Da Vinci.\n"
 
 abreMapa01 :: [String] -> IO()
 abreMapa01 opcoes = do
 
     mapM_ putStrLn opcoes
 
+    putStrLn "\n------------------------------------------------------------------------------------\n"
+
     input <- getLine
     if input == "1" then do
         clearScreen
         verLoja
-        abreMapa01 (removeOpcao "(1) Voltar a loja do Ferreiro Ferreira para *ver* os itens disponíveis." opcoes)
+        abreMapa01 (removeOpcao "(1) Voltar a loja do ferreiro Ferreira para *ver* os itens disponíveis." opcoes)
     else if input == "2" then do
         clearScreen
         putStrLn dialogoFerreira01
@@ -42,13 +38,13 @@ abreMapa01 opcoes = do
     else if input == "3" then do
         clearScreen
         putStrLn dialogoPadeiro01
-        abreMapa01 (removeOpcao "(3) Ir a praça da cidade." opcoes)
+        abreMapa01 (removeOpcao "(3) Ir à praça da cidade." opcoes)
     else if input == "4" then do
         clearScreen
         putStrLn cursoHistoria02
         sistemaGold
         putStrLn cursoAventura01
-        historiaPrincipal ["(1)Ganhar dinheiro","(2)Comprar poções com C.W.","(3)Visitar o ferreiro Ferreira","(4)Me garanto em enfrentar a I.A."]
+        historiaPrincipal ["(1) Ganhar dinheiro","(2) Comprar poções com C.W.","(3) Visitar o ferreiro Ferreira","(4) Me garanto em enfrentar a I.A."]
     else do
         putStrLn "Escreva uma opção válida."
         abreMapa01 opcoes
@@ -62,39 +58,45 @@ removeOpcao opcao (a:as)
 sistemaGold :: IO ()
 sistemaGold = do
 
-    putStrLn "(1)Trabalhar ajudando a carregar alguns suprimentos para um mercado mágico próximo.\n(2)Responder enigmas que as IAs utilizam para afirmar sua superioridade.\n(3)N.D.A."
+    putStrLn "(1) Trabalhar ajudando a carregar alguns suprimentos para um mercado mágico próximo.\n(2) Responder enigmas que as IAs utilizam para afirmar sua superioridade.\n(3) N.D.A."
+    putStrLn "\n------------------------------------------------------------------------------------\n"
 
     input <- getLine
     if input == "1" then do
+        clearScreen
         putStrLn explicacaoBasica01
-        putStrLn "QUANDO QUISER PARAR, APERTE QUALQUER TECLA."
+        putStrLn "*QUANDO QUISER PARAR, APERTE QUALQUER TECLA.*"
         conteudo <- readFile' "./src/pacote/Heroi.txt"
         let heanes = read conteudo :: Player
         sistemaGoldPassivoAux (gold heanes)
     else if input == "2" then do
-        putStrLn explicacaoBasica02
+        clearScreen
         sistemaGoldAtivoAux
-    else putStrLn "\nNão quer trabalhar hein, tudo bem. Boa sorte!"
+    else do
+        clearScreen
+        putStrLn "Não quer trabalhar hein, tudo bem. Boa sorte!"
 
 historiaPrincipal::[String]->IO()
 historiaPrincipal opcoes02 = do
 
-    putStrLn "O que deseja fazer agora que está de volta a cidade?"
+    putStrLn "O que deseja fazer agora que está de volta a cidade?\n"
     mapM_ putStrLn opcoes02
+    putStrLn "\n------------------------------------------------------------------------------------\n"
 
     opcaoJogador <- getLine
     if opcaoJogador == "1" then do
         clearScreen
+        putStrLn "Ganhar alguns mangos sempre é bom, talvez assim nosso herói não precise aderir a nenhuma greve. Como iremos angariar fundos?\n"
         sistemaGold
-        historiaPrincipal (removeOpcao "(1)Ganhar dinheiro" opcoes02)
+        historiaPrincipal (removeOpcao "(1) Ganhar dinheiro" opcoes02)
     else if opcaoJogador == "2" then do
         clearScreen
         abreLojaPocoesInicial
-        historiaPrincipal (removeOpcao "(2)Comprar poções com C.W." opcoes02)
+        historiaPrincipal (removeOpcao "(2) Comprar poções com C.W." opcoes02)
     else if opcaoJogador == "3" then do
         clearScreen
         abreLojaItensInicial
-        historiaPrincipal (removeOpcao "(3)Visitar o ferreiro Ferreira" opcoes02)
+        historiaPrincipal (removeOpcao "(3) Visitar o ferreiro Ferreira" opcoes02)
     else if opcaoJogador == "4" then do
         clearScreen
         combate01
